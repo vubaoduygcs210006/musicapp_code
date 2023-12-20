a44965d2be3edbfd72e5edc16c708e5d16bd6044
AMJASUS <vubaoduy2205@gmail.com>
2023-12-20 23:57:46 +0700

    Final Submit

--- src/components/MusicPlayer/Player.jsx
+++ src/components/MusicPlayer/Player.jsx
@@ -1,21 +1,18 @@
-/* eslint-disable jsx-a11y/media-has-caption */
 import React, { useRef, useEffect } from 'react';
 
 const Player = ({ activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate, onLoadedData, repeat }) => {
   const ref = useRef(null);
-  // eslint-disable-next-line no-unused-expressions
   if (ref.current) {
     if (isPlaying) {
       ref.current.play();
     } else {
       ref.current.pause();
     }
-  }
+  } 
 
   useEffect(() => {
     ref.current.volume = volume;
   }, [volume]);
-  // updates audio element only on seekTime change (and not on each rerender):
   useEffect(() => {
     ref.current.currentTime = seekTime;
   }, [seekTime]);
